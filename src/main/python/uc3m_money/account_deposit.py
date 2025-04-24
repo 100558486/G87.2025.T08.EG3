03d2956f19393f1a2b079bd6adae80f520ebcb01
100557612 <100557612@alumnos.uc3m.es>
2025-04-24 21:07:19 +0200

    Corrected several class docstrings and renamed a few variables for clarity, changes are commented on the code

--- src/main/python/uc3m_money/account_deposit.py
+++ src/main/python/uc3m_money/account_deposit.py
@@ -3,7 +3,8 @@ from datetime import datetime, timezone
 import hashlib
 
 class AccountDeposit():
-    """Class representing the information required for shipping of an order"""
+    """Class representing the information required for a deposit into an account."""
+    # Corrected class docstring
 
     def __init__(self,
                  to_iban: str,
@@ -13,7 +14,9 @@ class AccountDeposit():
         self.__to_iban = to_iban
         self.__deposit_amount = deposit_amount
         justnow = datetime.now(timezone.utc)
-        self.__deposit_date = datetime.timestamp(justnow)
+        # Renamed __deposit_date to __deposit_timestamp for clarity
+        self.__deposit_timestamp = datetime.timestamp(justnow)
+
 
     def to_json(self):
         """returns the object data in json format"""
@@ -21,18 +24,21 @@ class AccountDeposit():
                 "type": self.__type,
                 "to_iban": self.__to_iban,
                 "deposit_amount": self.__deposit_amount,
-                "deposit_date": self.__deposit_date,
+                "deposit_timestamp": self.__deposit_timestamp, # Updated name
                 "deposit_signature": self.deposit_signature}
 
-    def __signature_string(self):
-        """Composes the string to be used for generating the key for the date"""
-        return "{alg:" + str(self.__alg) +",typ:" + str(self.__type) +",iban:" + \
-               str(self.__to_iban) + ",amount:" + str(self.__deposit_amount) + \
-               ",deposit_date:" + str(self.__deposit_date) + "}"
+    def _get_signature_string(self): # Renamed from __signature_string,
+        # use single underscore convention
+        """Composes the string to be used for generating the deposit signature."""
+        # Improved docstring
+        # Using f-string for slightly better readability
+        return (f"{{alg:{self.__alg},typ:{self.__type},iban:{self.__to_iban},"
+                f"amount:{self.__deposit_amount},deposit_date:{self.__deposit_timestamp}}}")
+        # Updated name
 
     @property
     def to_iban(self):
-        """Property that represents the product_id of the patient"""
+        """Property representing the IBAN the deposit is made to.""" # Corrected docstring
         return self.__to_iban
 
     @to_iban.setter
@@ -41,22 +47,22 @@ class AccountDeposit():
 
     @property
     def deposit_amount(self):
-        """Property that represents the order_id"""
+        """Property representing the amount deposited.""" # Corrected docstring
         return self.__deposit_amount
+
     @deposit_amount.setter
     def deposit_amount(self, value):
         self.__deposit_amount = value
 
     @property
-    def deposit_date(self):
-        """Property that represents the phone number of the client"""
-        return self.__deposit_date
-    @deposit_date.setter
-    def deposit_date( self, value ):
-        self.__deposit_date = value
-
+    def deposit_timestamp(self): # Renamed property to match attribute
+        """Read-only property representing the timestamp when the deposit object was created."""
+        # Corrected docstring
+        return self.__deposit_timestamp
+    # Removed setter for timestamp as it's set internally on creation
 
     @property
     def deposit_signature( self ):
-        """Returns the sha256 signature of the date"""
-        return hashlib.sha256(self.__signature_string().encode()).hexdigest()
+        """Returns the sha256 signature of the deposit data.""" # Corrected the docstring
+        # Changed call to use the renamed private method
+        return hashlib.sha256(self._get_signature_string().encode()).hexdigest()
